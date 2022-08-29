8f6b757137571843a6aeadcdd1889f8d7d5609f9
tezzeretvess <johannes.sigmund@gymburgdorf.ch>
2022-08-29 11:57:24 +0200

    Javaskript

--- 43_functions.js
+++ 43_functions.js
@@ -13,8 +13,10 @@ const c = getHypotenuse(3, 4)  // c is now: 5
 
 // convert to seconds
 function getSeconds(hours, minutes, seconds) {
-	//@TODO
-	return 
+	let result = seconds
+	result += hours*3600
+	result += minutes*60
+	return result
 }
 const oneLesson = getSeconds(0, 45, 0) // should return 2700
 
@@ -22,8 +24,8 @@ const oneLesson = getSeconds(0, 45, 0) // should return 2700
 
 //Create HTML heading tag
 function createHeading(text) {
-	//@TODO
-	return 
+	return `"<h1>${text}"</h1>`
+	
 }
 const heading = createHeading("Welcome") // should return "<h1>Welcome</h1>"
 
@@ -31,8 +33,8 @@ const heading = createHeading("Welcome") // should return "<h1>Welcome</h1>"
 
 //Create SVG rect
 function createSvgRect(x, y, w, h) {
-	//@TODO
-	return 
+	
+	return `<rect x='${x}' y='${y}' width='${w}' height='${h}'></rect>`
 }
 const svgRect = createSvgRect(100, 200, 300, 400) // should return "<rect x='100' y='200' width='300' height='400'></rect>"
 
@@ -40,8 +42,8 @@ const svgRect = createSvgRect(100, 200, 300, 400) // should return "<rect x='100
 
 // Find median of 3 values
 function medianOf3(a, b, c) {
-	//@TODO
-	return 
+	
+	return (a+b+c)/3
 }
 const m = medianOf3(8, 11, 9) // should return 9
 
@@ -49,8 +51,8 @@ const m = medianOf3(8, 11, 9) // should return 9
 
 // Find average of 3 values
 function averageOf3(a, b, c) {
-	//@TODO
-	return 
+	
+	return Math.averageOf3(a,b,c)
 }
 const a = averageOf3(4, 11, 9) // should return 8
 
@@ -58,7 +60,7 @@ const a = averageOf3(4, 11, 9) // should return 8
 
 // Find average of array
 function getAverage(array) {
-	//@TODO
+	
 	return 
 }
 const b = getAverage([4, 11, 9, 2, 9]) // should return 7
@@ -67,8 +69,8 @@ const b = getAverage([4, 11, 9, 2, 9]) // should return 7
 
 // Solve ax^2 + bx + c = 0
 function solveQuadratic(a, b, c) {
-	//@TODO
-	return 
+	
+	return Math.solveQuadratic(a,b,c)
 }
 const solutions = solveQuadratic(1, 0, -16) // should return [-4, 4]
 const solution = solveQuadratic(1, -2, 1) // should return [1]
